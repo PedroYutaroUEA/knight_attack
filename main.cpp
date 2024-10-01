b404c4ce79bc651f35e2b5b2338e790a4bd810eb
PedroYutaroUEA <pymmn.snf23@uea.edu.br>
2024-10-01 18:05:56 -0400

    almost done i guess

--- main.cpp
+++ main.cpp
@@ -1,189 +1,183 @@
 #include <iostream>
 #include <list>
 #include <limits>
-#include <vector>
 #include <queue>
+#include <vector>
 
-#define INF std::numeric_limits<uint>::infinity()
+#define INF std::numeric_limits<uint>::max() 
+#define TAB_SIZE 8
 
 using namespace std;
 
-typedef unsigned int Vertex;
-typedef unsigned int uint;
+typedef unsigned int Vertex, uint;
 
 enum Color { WHITE, GRAY, BLACK };
+
+const int moves[8][2] = {
+  {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+  {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+};
 
 class Node {
 private:
   Vertex value;
   Node *previous;
   Color color;
-  uint t0, t, dist;
+  uint dist;
 public:
-Node() : value(-1), previous(nullptr), color(WHITE), t0(0), t(0) {
-  dist = INF;
-}
-Node(Vertex v) : value(v), previous(nullptr), color(WHITE), t0(0), t(0) {
-  dist = INF;
-}
+  Node() : value(-1), previous(nullptr), color(WHITE), dist(INF) {}
+  Node(Vertex v) : value(v), previous(nullptr), color(WHITE), dist(INF) {}
+  
   void setValue(Vertex v) { value = v; }
   Vertex getValue() const { return value; }
   void setColor(Color c) { color = c; }
   Color getColor() const { return color; }
   void setPrevious(Node *prev) { previous = prev; }
   Node *getPrevious() const { return previous; }
-  void setT(uint t) { this->t = t; }
-  uint getT() const { return t; }
-  void setT0(uint t0) { this->t0 = t0; }
-  uint getT0() const { return t0; }
   void setDist(uint dist) { this->dist = dist; }
   uint getDist() const { return dist; }
-  // for remove_edge()
+  
   bool operator==(const Node &other) const { return value == other.value; }
 };
 
-template <typename T> class Graph {
+class Graph {
 private:
-  uint num_vertices;
-  uint num_edges;
-  list<T> *adj;
-  vector<T> nodes;
+  uint num_edges, num_vertices;
+  list<Node> *adj;
+  vector<Node> nodes;
 public:
-  Graph(uint num_vertices) : num_vertices(num_vertices), num_edges(0) {
+  Graph() : num_edges(0), num_vertices(TAB_SIZE * TAB_SIZE) {
     nodes.resize(num_vertices + 1);
-    for (uint i = 0; i < num_vertices + 1; i++) {
-      nodes[i] = T(i);
-    }
-    adj = new list<T>[num_vertices + 1];
+    for (uint i = 0; i < num_vertices + 1; i++)
+      nodes[i] = Node(i);
+    adj = new list<Node>[num_vertices + 1];
   }
+
   ~Graph() { delete[] adj; }
-  uint get_num_vertices() const { return num_vertices; }
-  uint get_num_edges() const { return num_edges; }
-  void add_edge(Vertex u, Vertex v) {
-    T Node_u(u);
-    T Node_v(v);
-    adj[u].push_back(Node_v);
-    adj[v].push_back(Node_u);
+  
+  Vertex posToVertex(uint i, uint j) const {
+    return i * TAB_SIZE + j;
+  }
+
+  void addEdge(uint Ui, uint Uj, uint Vi, uint Vj) {
+    Vertex u = posToVertex(Ui, Uj);
+    Vertex v = posToVertex(Vi, Vj);
+    adj[u].push_back(nodes[v]);
+    adj[v].push_back(nodes[u]);
     num_edges++;
   }
-  list<T>& get_adj(Vertex vertex) const {
+
+  list<Node>& getAdj(uint i, uint j) {
+    Vertex vertex = posToVertex(i, j);
     return adj[vertex];
   }
-  void remove_edge(Vertex u, Vertex v) {
-    T Node_u(u);
-    T Node_v(v);
-    adj[u].remove(Node_v);
-    adj[v].remove(Node_u);
-    num_edges--;
+
+  Node& getNode(uint i, uint j) {
+    Vertex v = posToVertex(i, j);
+    return nodes[v];
   }
 
-  void show_graph() const {
-    cout << "num_vertices: " << num_vertices << endl;
-    cout << "num_edges: " << num_edges << endl;
-    for (Vertex v = 0; v < num_vertices; v++) {
-      cout << v << ": ";
-      list<T> adj_list = get_adj(v);
-      for (const auto &vertex : adj_list)
-        cout << vertex.getValue() << ", ";
-      cout << endl;
+  bool insideBoard(int i, int j) {
+    return i >= 0 && i < TAB_SIZE && j >= 0 && j < TAB_SIZE;
+  }
+
+  void createHorseMovesGraph(uint horse_i, uint horse_j) {
+    queue<pair<uint, uint>> Q;
+    vector<vector<bool>> visited(TAB_SIZE, vector<bool>(TAB_SIZE, false));
+    
+    Q.push({horse_i, horse_j});
+    visited[horse_i][horse_j] = true;
+    
+    while (!Q.empty()) {
+      pair<uint, uint> pos = Q.front();
+      Q.pop();
+      
+      for (auto move : moves) {
+        int new_i = pos.first + move[0];
+        int new_j = pos.second + move[1];
+        
+        if (insideBoard(new_i, new_j) && !visited[new_i][new_j]) {
+          addEdge(pos.first, pos.second, new_i, new_j);
+          Q.push({new_i, new_j});
+          visited[new_i][new_j] = true;
+        }
+      }
     }
-  }
-
-  void printVertexDegreeSum() {
-    int sum = 0;
-    for (Vertex v = 1; v <= num_vertices; v++) {
-      int degree = get_adj(v).size();
-      sum += degree;
-    }
-    cout << "Sum of degrees: " << sum << endl;
-  }
-
-  bool isComplete() const {
-    for (Vertex v = 1; v <= num_vertices; v++) {
-      int degree = get_adj(v).size();
-      if (degree + 1 != num_vertices)
-        return false;
-    }
-    return true;
-  }
-
-  vector<T> getNodes() {
-    return nodes;
   }
 };
 
-template <typename T> void DFS(Graph<T> &g) {
-  uint time = 0;
-  for (Vertex u = 0; u < g.get_num_vertices(); u++) {
-    for (auto &node_u : g.get_adj(u))
-      if (node_u.getColor() == WHITE)
-        DFS_visit(g, node_u, time);
-  }
+pair<int, int> posToCoordinate(const string &pos) {
+  uint i = pos[1] - '1';
+  uint j = pos[0] - 'a';
+  return {i, j};
 }
 
-template <typename T> void DFS_visit(Graph<T> &g, T &node_u, uint &time) {
-  node_u.setColor(GRAY);
-  ++time;
-  node_u.setT0(time);
-  cout << "t0: " << node_u.getT0() << "; vertex: " << node_u.getValue() << endl;
-  
-  for (auto &node_v : g.get_adj(node_u.getValue())) {
-    if (node_v.getColor() == WHITE) {
-      node_v.setPrevious(&node_u);
-      DFS_visit(g, node_v, time);
+uint BFS(Graph &g, Node &root, pair<int, int> &kingPos) {
+  queue<Node*> Q;
+  for (int i = 0; i < TAB_SIZE; ++i) {
+    for (int j = 0; j < TAB_SIZE; ++j) {
+      Node &node = g.getNode(i, j);
+      node.setColor(WHITE);
+      node.setDist(INF);
+      node.setPrevious(nullptr);
     }
   }
-
-  node_u.setColor(BLACK);
-  ++time;
-  node_u.setT(time);
-  cout << "t: " << node_u.getT() << "; vertex: " << node_u.getValue() << endl;
-}
-
-template <typename T> void BFS(Graph<T> &g, T &root) {
-  for (T &node : g.getNodes()) {
-    node.setColor(WHITE);
-    node.setDist(INF);
-    node.setPrevious(nullptr);
-  }
+  
   root.setColor(GRAY);
   root.setDist(0);
-  queue<T*> Q;
   Q.push(&root);
   
   while (!Q.empty()) {
-    T *node_u = Q.front();
+    Node *node_u = Q.front();
     Q.pop();
     
-    for (auto &node_v : g.get_adj(node_u->getValue())) {
-      cout << node_v.getColor() << endl;
+    Vertex u = node_u->getValue();
+    int x = u / TAB_SIZE;
+    int y = u % TAB_SIZE;
+
+    if (make_pair(x, y) == kingPos) {
+      return node_u->getDist();
+    }
+
+    for (auto &node_v : g.getAdj(x, y)) {
       if (node_v.getColor() == WHITE) {
         node_v.setColor(GRAY);
         node_v.setDist(node_u->getDist() + 1);
         node_v.setPrevious(node_u);
         Q.push(&node_v);
-        cout << "dist: " << node_v.getDist() << "; vertex: " << node_v.getValue() << endl;
       }
     }
     node_u->setColor(BLACK);
   }
+  return INF;
 }
 
 int main() {
-  uint n_edges, n_vertices;
-  cin >> n_vertices >> n_edges;
-  Graph<Node> g(n_vertices);
-  Vertex u, v;
-  for (uint i = 0; i < n_edges; i++) {
-    cin >> u >> v;
-    g.add_edge(u, v);
+  int tests;
+  cin >> tests;
+  vector<string> positions(5);
+  vector<vector<string>> rounds(tests, positions);
+
+  for (int t = 0; t < tests; ++t)
+    for (int i = 0; i < 5; ++i)
+      cin >> rounds[t][i];
+
+  for (int t = 0; t < tests; ++t) {
+    pair<int, int> horses[4], king;
+    for (int i = 0; i < 4; ++i)
+      horses[i] = posToCoordinate(rounds[t][i]);
+    king = posToCoordinate(rounds[t][4]);
+
+    for (auto &horse : horses) {
+      Graph g;
+      g.createHorseMovesGraph(horse.first, horse.second);
+      uint u_i = horse.first;
+      uint u_j = horse.second;
+      Node &horseNode = g.getNode(u_i, u_j);
+      uint dist = BFS(g, horseNode, king);
+      cout << dist - 1 << " ";
+    }
   }
-  g.show_graph();
-  cout << "<==== DFS ====>" << endl;
-  DFS(g);
-  
-  cout << "<==== BFS ====>" << endl;
-  Node root = g.getNodes()[1];
-  BFS(g, root);
+
   return 0;
 }
